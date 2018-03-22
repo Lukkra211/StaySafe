646049d85cc539ac5ee081365edff3e259fca019
Pavel Balusek <mr.p.balusek@gmail.com>
2018-03-22 15:19:54 +0100

    Make page title changed when Item in listView is cliked

--- app/src/main/java/com/example/pc/staysafe/DangerActivity.java
+++ app/src/main/java/com/example/pc/staysafe/DangerActivity.java
@@ -1,15 +1,10 @@
 package com.example.pc.staysafe;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,12 +19,15 @@ import java.util.ArrayList;
 public class DangerActivity extends AppCompatActivity {
 
     private TestModel testModel = new TestModel();
+    private TextView pageTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danger);
+
         ArrayList<Article> arts = testModel.getArticles(TestModel.DangerType.REAL);
+        pageTitle = findViewById(R.id.pageTitle);
 
         for (Article article : arts) {
 
@@ -41,9 +39,18 @@ public class DangerActivity extends AppCompatActivity {
             }
         }
 
-        ListView testArticles = findViewById(R.id.TestArticlesContainer);
+        final ListView testArticles = findViewById(R.id.TestArticlesContainer);
 
         ArticlesListAdapter testArticlesAdapter = new ArticlesListAdapter(this, arts);
         testArticles.setAdapter(testArticlesAdapter);
+
+        //Make Item in listview clickable and change title of page when cliked
+        testArticles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Article text = (Article) parent.getItemAtPosition(position);
+                pageTitle.setText(text.getTitle());
+            }
+        });
     }
 }
